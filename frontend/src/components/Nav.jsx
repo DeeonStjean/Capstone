78740120e08a09c7b67499e0745c693c72146aef
DeeonStjean <deeonstjean101@gmail.com>
2024-10-10 11:17:46 -0400

    updating navbar

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -4,6 +4,9 @@ export default function Nav (props) {
   return (
     <div className="topnav">
       <Link to="/">Home</Link>
+      <Link to="/search">search</Link>
+      <Link to="/">Home</Link>
+      <Link to="/signup">signup</Link>
     </div>
   );
 }
