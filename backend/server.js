c2b01c4965b94073cdef2ae27bf4ad80eb431fc1
DeeonStjean <deeonstjean101@gmail.com>
2024-10-11 14:09:26 -0400

    updating backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,26 @@
 // bring in express for our server setup
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+// bring in the function that will make the connection to the database
+import connectDB from './config.js'
+
+const express = require("express");
 // create our express app
-const app = express()
+const app = express();
+
+// setup a cors middleware for our express app
+app.use(cors());
+// data from client stored in request.body and formatted as json
+app.use(express.json());
+
 // choosing a port 
-const PORT = 5050
-// at least one basic route for testing purposes
-app.get('/test', (req, res) => {
-    res.json('Hello (from Server)!')
-})
-// setup our server to listen on a specific port
-app.listen(PORT, () => {
-    console.log('Listening on port: ' + PORT)
-})
+const port = 5000;
+  
+app.post('/weather', (req, res) => {
+    // Save weather data to database
+});
+  
+app.listen(port,() => {
+    console.log("Port {server running on port");
+});
