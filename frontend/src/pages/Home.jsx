ce7d9b670ba6070f6df10ae9943698958774d8cf
DeeonStjean <deeonstjean101@gmail.com>
2024-10-14 17:30:42 -0400

    connect to api

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,4 +5,23 @@ const apikey = weatherkey;
 export default function Home(props){
   const [city, setCity] = useState('');
   const [weatherData, setWeatherData] = useState(null);
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
+        getWeatherData(url);
+      });
+    }
+  }, []);
+  // get data from weather api
+  const getWeatherData = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  };
 }
