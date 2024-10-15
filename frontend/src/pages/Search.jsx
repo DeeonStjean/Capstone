4db2397580fbecc7cef0a926b9c092a981fe502e
DeeonStjean <deeonstjean101@gmail.com>
2024-10-14 23:57:37 -0400

    add a function to search by city

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -3,6 +3,23 @@ import weatherkey from "../weatherconfig";
 const apikey = weatherkey;
 export default function Search(props){
     const [city, setCity] = useState('');
+    const [weatherData, setWeatherData] = useState(null);
+    const searchByCity = async () => {
+        try {
+          const urlsearch = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+          const response = await fetch(urlsearch);
+          weatherData = await response.json();
+          console.log(weatherData);
+          weatherReport(weatherData);
+          setWeatherData(weatherData);
+          // Send data to backend for storage
+          //saveWeatherData(data);
+        } catch (error) {
+          console.error('Error fetching weather data:', error);
+        }
+        setCity('');
+      };
+    
     return(
         <div className="background">
             <div className="header">
