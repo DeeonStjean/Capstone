000dddd28fd2293b6b407163f4d435a293a57197
DeeonStjean <deeonstjean101@gmail.com>
2024-10-14 22:47:06 -0400

    updating search page

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -2,5 +2,25 @@ import {useState, useEffect} from "react";
 import weatherkey from "../weatherconfig";
 const apikey = weatherkey;
 export default function Search(props){
-    
+    const [city, setCity] = useState('');
+    return(
+        <div className="background">
+            <div className="header">
+                <h1>WEATHER APP</h1>
+                <div>
+                    <input
+                    type="text"
+                    name=""
+                    id="input"
+                    placeholder="Enter city name"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                    />
+                    <button id="search" /*onClick={searchByCity}*/>
+                        Search
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 }
